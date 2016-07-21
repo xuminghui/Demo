6b8276c43630fc33ae1188d6801f398b1c2c68a0
xuminghui <xuminghui18610740807@gmail.com>
2016-07-21 12:54:17 +0800

    test

--- src/main/java/com/example/controller/RequestContentTypeController.java
+++ src/main/java/com/example/controller/RequestContentTypeController.java
@@ -1,16 +1,106 @@
 package com.example.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.entity.Apple;
 @Controller
 @RequestMapping("/testContentType")
 public class RequestContentTypeController {
+	/**
+	 * headers表示服务端只对请求头是headers的类型进行处理。
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping(value = "ContentType", method = RequestMethod.GET,headers = "Content-Type=text/html;charset=UTF-8") 
 	public  String showData(HttpServletRequest request){
 		System.out.println("contentType: "+request.getContentType());
 		return "testAny";
 	}
+	/**
+	 * 通过@RequestBody将JSON数据组装成实体对象
+	 * @param apple
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "ContentTypeForJson", method = RequestMethod.POST,headers = "Content-Type=application/json;charset=UTF-8")
+	public  String dealWithJson(@RequestBody Apple apple, HttpServletRequest request){
+		System.out.println("contentType: "+request.getContentType());
+		System.out.println(apple.getColor()+" : "+apple.getName());
+		return "testAny";
+	}
+	/**
+	 * 响应的contentType
+	 * @param response
+	 * @throws IOException
+	 */
+	@RequestMapping(value="contentTypeForResponse")  
+	public void contentTypeForResponse(HttpServletResponse response) throws IOException {  
+	    //①表示响应的内容区数据的媒体类型为html格式，且编码为utf-8(客户端应该以utf-8解码)  
+	    response.setContentType("text/html;charset=utf-8");  
+	    //②写出响应体内容  
+	    response.getWriter().write("<font style='color:red'>hello</font>");  
+	} 
+	/**
+	 * 处理contentType为xml的类型
+	 * @param response
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "ContentTypeForXml", headers = "Accept=application/xml")  
+	public void contentTypeForXml(HttpServletResponse response) throws IOException {  
+	    //①表示响应的内容区数据的媒体类型为xml格式，且编码为utf-8(客户端应该以utf-8解码)  
+	    response.setContentType("application/xml;charset=utf-8");  
+	    //②写出响应体内容  
+	    String xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";  
+	    xmlData += "<user><username>zhang</username><password>123</password></user>";  
+	    response.getWriter().write(xmlData);  
+	} 
+	/**
+	 * 验证请求contentType不匹配情况
+	 * @param response
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "ContentTypeForCheck", headers = "ContentType=application/xml")  
+	public void check(HttpServletResponse response) throws IOException {  
+	    //①表示响应的内容区数据的媒体类型为xml格式，且编码为utf-8(客户端应该以utf-8解码)  
+	    response.setContentType("application/xml;charset=utf-8");  
+	    //②写出响应体内容  
+	    String xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";  
+	    xmlData += "<user><username>zhang</username><password>123</password></user>";  
+	    response.getWriter().write(xmlData);  
+	} 
+	
+	/**
+	 * 表示服务器可接受的contentType类型，通过request的contentType头匹配，相当于上面的headers="ContentType=text/html"
+	 * @param response
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "ContentTypeForConsumes", consumes={"text/html"})  
+	public void consumes(HttpServletResponse response) throws IOException {  
+	    //①表示响应的内容区数据的媒体类型为text/html格式，且编码为utf-8(客户端应该以utf-8解码)  
+	   // response.setContentType("text/hxml;charset=utf-8");  
+	    //②写出响应体内容  
+	    String htmlData = "<font style='color:red'>hello</font>";  
+	    response.getWriter().write(htmlData);  
+	} 
+	/**
+	 * 表示功能处理方法将产生text/html类型的数据，是和request头中的Accept进行匹配的
+	 * @param response
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "ContentTypeForProduces", produces={"text/html"})  
+	public void produces(HttpServletResponse response) throws IOException {  
+	    //①表示响应的内容区数据的媒体类型为text/html格式，且编码为utf-8(客户端应该以utf-8解码)  
+	    response.setContentType("text/hxml;charset=utf-8");  
+	    //②写出响应体内容  
+	    String htmlData = "<font style='color:red'>hello</font>";  
+	    response.getWriter().write(htmlData);  
+	} 
 }
--- src/test/java/com/example/controllerTest/TestRequestContentType.java
+++ src/test/java/com/example/controllerTest/TestRequestContentType.java
@@ -2,9 +2,12 @@ package com.example.controllerTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -52,7 +55,7 @@ public class TestRequestContentType {
 	}
 	//利用SPRING提供的HTTP 客户端API
 	@Test
-	public void testHttpRequest() throws IOException, URISyntaxException{
+	public void testHttpRequestForTextHtml() throws IOException, URISyntaxException{
 		//请求的地址  
 		String url = "http://localhost:"+port+"/springboot/testContentType/ContentType";  
 		//①创建Http Request(内部使用HttpURLConnection)  
@@ -64,8 +67,130 @@ public class TestRequestContentType {
 		String jsonData = "{\"username\":\"zhang\", \"password\":\"123\"}";  
 		request.getBody().write(jsonData.getBytes("gbk"));  */
 		//④发送请求并得到响应  
-		ClientHttpResponse response = request.execute();  
+		ClientHttpResponse response = request.execute(); 
+		//获取响应体的编码方式
+		Charset charset = response.getHeaders().getContentType().getCharSet();
+		System.out.println("charset: "+charset.name());
 		System.out.println(response.getStatusCode());  
 	}
+	//利用SPRING提供的HTTP 客户端API
+		@Test
+		public void testHttpRequestForJson() throws IOException, URISyntaxException{
+			//请求的地址  
+			String url = "http://localhost:"+port+"/springboot/testContentType/ContentTypeForJson";  
+			//①创建Http Request(内部使用HttpURLConnection)  
+			ClientHttpRequest request = new SimpleClientHttpRequestFactory().createRequest(new URI(url), HttpMethod.POST);  
+			//②设置请求头的内容类型头和内容编码（UTF-8）  
+			request.getHeaders().set("Content-Type", "application/json;charset=utf-8");  
+			
+			//③以GBK编码写出请求内容体  
+			String jsonData = "{\"name\":\"zhang\", \"color\":\"123\"}";  
+			request.getBody().write(jsonData.getBytes("utf-8"));
+			//④发送请求并得到响应  
+			ClientHttpResponse response = request.execute();  
+			System.out.println(response.getStatusCode());  
+		}
+		/**
+		 * 测试resonse的ContentType
+		 * @throws IOException
+		 * @throws URISyntaxException
+		 */
+		@Test
+		public void testHttpForResponseContentType() throws IOException, URISyntaxException{
+			//请求的地址  
+			String url = "http://localhost:"+port+"/springboot/testContentType/contentTypeForResponse";  
+			//①创建Http Request(内部使用HttpURLConnection)  
+			ClientHttpRequest request = new SimpleClientHttpRequestFactory().createRequest(new URI(url), HttpMethod.GET);  
+			//②设置请求头的内容类型头和内容编码（UTF-8）  
+			request.getHeaders().set("Content-Type", "text/html;charset=utf-8");  
+			//④发送请求并得到响应  
+			ClientHttpResponse response = request.execute();  
+			//获取响应体的编码方式
+			Charset charset = response.getHeaders().getContentType().getCharSet();
+			
+			/*String body=readStream(response.getBody());*/
+			InputStream is = response.getBody();
+			//获取响应内容的长度
+		    byte bytes[] = new byte[(int)response.getHeaders().getContentLength()];  
+		    is.read(bytes);  
+		    String jsonData = new String(bytes, charset);  
+		    System.out.println("charset : " + charset + ", json data : " + jsonData);  
+			System.out.println(response.getStatusCode());  
+		}
+		@Test
+		public void testHttpForApplicationXml() throws IOException, URISyntaxException{
+			//请求的地址  
+			String url = "http://localhost:"+port+"/springboot/testContentType/ContentTypeForXml";  
+			//①创建Http Request(内部使用HttpURLConnection)  
+			ClientHttpRequest request = new SimpleClientHttpRequestFactory().createRequest(new URI(url), HttpMethod.POST);  
+			//设置客户端可接受的媒体类型（即需要什么类型的响应体数据）
+			 request.getHeaders().set("Accept", "application/xml");  
+			//④发送请求并得到响应  
+			ClientHttpResponse response = request.execute();  
+			//获取响应体的编码方式
+			Charset charset = response.getHeaders().getContentType().getCharSet();
+			
+			/*String body=readStream(response.getBody());*/
+			InputStream is = response.getBody();
+			//获取响应内容的长度
+		    byte bytes[] = new byte[(int)response.getHeaders().getContentLength()];  
+		    is.read(bytes);  
+		    String jsonData = new String(bytes, charset);  
+		    System.out.println("charset : " + charset + ", XML data : " + jsonData);  
+			System.out.println(response.getStatusCode());  
+		}
+		//验证ContentType不匹配情况
+		@Test
+		public void testHttpCheck() throws IOException, URISyntaxException{
+			//请求的地址  
+			String url = "http://localhost:"+port+"/springboot/testContentType/ContentTypeForCheck";  
+			//①创建Http Request(内部使用HttpURLConnection)  
+			ClientHttpRequest request = new SimpleClientHttpRequestFactory().createRequest(new URI(url), HttpMethod.POST);  
+			//设置客户端可接受的媒体类型（即需要什么类型的响应体数据）
+			 request.getHeaders().set("Content-Type", "text/html");  
+			//④发送请求并得到响应  
+			ClientHttpResponse response = request.execute();  
+			System.out.println(response.getStatusText());
+		}
+		//测试consumes：服务端可消费的MIME类型
+		@Test
+		public void testConsumes() throws IOException, URISyntaxException{
+			//请求的地址  
+			String url = "http://localhost:"+port+"/springboot/testContentType/ContentTypeForConsumes";  
+			//①创建Http Request(内部使用HttpURLConnection)  
+			ClientHttpRequest request = new SimpleClientHttpRequestFactory().createRequest(new URI(url), HttpMethod.POST);  
+			//设置客户端可接受的媒体类型（即需要什么类型的响应体数据） 注意写法是Content-Type
+			 request.getHeaders().set("Content-Type", "text/html");
+			//④发送请求并得到响应  
+			ClientHttpResponse response = request.execute();  
+			System.out.println(response.getStatusText());
+		}
+		//测试produces客户端可接受的MIME类型 ，根据request头的Accept匹配
+		@Test
+		public void testProduces() throws IOException, URISyntaxException{
+			//请求的地址  
+			String url = "http://localhost:"+port+"/springboot/testContentType/ContentTypeForProduces";  
+			//①创建Http Request(内部使用HttpURLConnection)  
+			ClientHttpRequest request = new SimpleClientHttpRequestFactory().createRequest(new URI(url), HttpMethod.GET);  
+			//设置客户端可接受的媒体类型（即需要什么类型的响应体数据） 注意写法是Content-Type
+			 request.getHeaders().set("Accept", "text/html");
+			//④发送请求并得到响应  
+			ClientHttpResponse response = request.execute();  
+			System.out.println(response.getStatusText());
+		}
+		
+		private String readStream(InputStream inputStream) throws IOException{
+			ByteArrayOutputStream outSteam = new ByteArrayOutputStream();  
+			byte[] buffer=new byte[1024];
+			int len=-1;
+			while((len=inputStream.read(buffer))!=-1){
+				outSteam.write(buffer, 0, len);
+			}
+			outSteam.close();  
+			inputStream.close();  
+		    byte[] bytes=outSteam.toByteArray();
+		    String content=new String(bytes,"UTF-8");
+		    return content;
+		}
 
 }
